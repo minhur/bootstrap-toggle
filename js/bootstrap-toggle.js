983bf45016d7abb00037362ac17d7d70f0c76119
Joshua Hogendorn <jhogendorn@gmail.com>
2013-11-12 19:28:38 +1000

    $input now fires change event

--- js/bootstrap-toggle.js
+++ js/bootstrap-toggle.js
@@ -5,17 +5,18 @@
 * This content is released under the MIT License.
 */
 !function ($) {
-	$(document).on('click.bootstrap-toggle', '[data-toggle^=toggle]', function(e) {
+	$(document).on('click.bootstrap-toggle', '[data-toggle^=toggle]', function() {
 		var $toggle = $(this);
 		var $input = $(this).find('input[type=checkbox]');
 		if ($toggle.hasClass('off')) {
-			$toggle.attr('class', 'toggle ' + $toggle.find('.toggle-on').attr('class').replace(/toggle-on/g,''))
+			$toggle.attr('class', 'toggle ' + $toggle.find('.toggle-on').attr('class').replace(/toggle-on/g,''));
 			$input.prop('checked', true);
 			$toggle.removeClass('off');
 		} else {
-			$toggle.attr('class', 'toggle ' + $toggle.find('.toggle-off').attr('class').replace(/toggle-off/g,''))
+			$toggle.attr('class', 'toggle ' + $toggle.find('.toggle-off').attr('class').replace(/toggle-off/g,''));
 			$input.prop('checked', false);
 			$toggle.addClass('off');
 		}
+    $input.change();
 	});
 }(window.jQuery);
