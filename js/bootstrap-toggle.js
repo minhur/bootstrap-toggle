26afdd6bcd1bee6599cf5f0f6620684a3a6bb65c
Jaroslav Hranička <hranicka@outlook.com>
2015-06-10 17:43:01 +0200

    Preserve checkbox title attribute [Closes #46]

--- js/bootstrap-toggle.js
+++ js/bootstrap-toggle.js
@@ -63,6 +63,7 @@
 		var $toggle = $('<div class="toggle btn" data-toggle="toggle">')
 			.addClass( this.$element.prop('checked') ? this._onstyle : this._offstyle+' off' )
 			.addClass(size).addClass(this.options.style)
+			.attr('title', this.$element.prop('title'))
 
 		this.$element.wrap($toggle)
 		$.extend(this, {
