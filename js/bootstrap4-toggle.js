a8ad34077bc88dec684f29390bf5924a1bf8ba69
GitBrent <gitbrent@users.noreply.github.com>
2019-01-03 21:47:06 -0600

    updated version to 3.5.0-beta

--- js/bootstrap4-toggle.js
+++ js/bootstrap4-toggle.js
@@ -1,6 +1,6 @@
 /*\
 |*| ========================================================================
-|*| Bootstrap Toggle: bootstrap4-toggle.js v3.4.0
+|*| Bootstrap Toggle: bootstrap4-toggle.js v3.5.0-beta
 |*| https://gitbrent.github.io/bootstrap-toggle/
 |*| ========================================================================
 |*| Copyright 2018-2019 Brent Ely
@@ -20,7 +20,7 @@
 		this.render()
 	}
 
-	Toggle.VERSION  = '3.4.0-beta'
+	Toggle.VERSION  = '3.5.0-beta'
 
 	Toggle.DEFAULTS = {
 		on: 'On',
