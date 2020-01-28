a9e00494d99fed07dfeb94ace080afdc6b6383e9
Hassadee Pimsuwan <h.pimsuwan@gmail.com>
2020-01-28 12:28:47 +0700

    Added support for Bootstrap 4
    Updated class name to match with Bootstrap 4 styles;

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const { src, dest, watch, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-csso');
+const concat = require('gulp-concat');
+const uglifyes = require('uglify-es');
+const composer = require('gulp-uglify/composer');
+const uglify = composer(uglifyes, console);
+
+function css() {
+	return src('./scss/*.scss')
+		.pipe(sass())
+		.pipe(dest('./css/'));
+}
+
+function css_minify() {
+	return src('./scss/*.scss')
+		.pipe(sass())
+		.pipe(minifyCSS())
+		.pipe(dest('./css/'));
+}
+
+function js() {
+	return src(['./js/bootstrap4-toggle.js'], { sourcemaps: true })
+		.pipe(uglify())
+		.pipe(concat('bootstrap4-toggle.min.js'))
+		.pipe(dest('./js/', { sourcemaps: true }));
+}
+
+function watchFiles() {
+  watch("./scss/**/*", css_minify)
+	watch("./scss/**/*", css);
+	watch("./js/bootstrap4-toggle.js", js);
+}
+
+exports.js = js;
+exports.css = css;
+exports.css_minify = css_minify;
+exports.watch = watch;
+exports.default = parallel(css, css_minify, js, watchFiles);
